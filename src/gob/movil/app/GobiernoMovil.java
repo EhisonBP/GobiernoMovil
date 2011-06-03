ad562114c7cfe12dd52ff14bd41766ff810bbc8e
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-02 22:01:09 -0430

    Agregando nuevo método para mostrar el menú de opciones en la pantalla principal de la aplicación y además de un método para recibir la clase de la Activity que deberá mostrarse a continuación, reduciendo código al implementarse.

--- src/gob/movil/app/GobiernoMovil.java
+++ src/gob/movil/app/GobiernoMovil.java
@@ -1,14 +1,48 @@
 package gob.movil.app;
 
 import gob.movil.R;
+import gob.movil.info.About;
+import gob.movil.info.Help;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GobiernoMovil extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.about:
+			showActivity(About.class);
+			return true;
+		case R.id.help:
+			showActivity(Help.class);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void showActivity(Class<?> c) {
+		Intent intent = new Intent();
+		intent.setComponent(new ComponentName(this, c));
+		startActivity(intent);
+	}
 }
