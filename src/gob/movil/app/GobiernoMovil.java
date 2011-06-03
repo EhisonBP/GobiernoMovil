97e92f3b29c7977eeefb9b848cd215672f1ff3e1
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-02 20:47:28 -0430

    Creando el primer archivo de la aplicación

---
+++ src/gob/movil/app/GobiernoMovil.java
@@ -0,0 +1,14 @@
+package gob.movil.app;
+
+import gob.movil.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GobiernoMovil extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
