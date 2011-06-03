882915a2530c32e15c6ad7cf3428c62e24fdcee6
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-02 21:36:50 -0430

    Agregando el método para mostrar la aplicación en pantalla.

--- src/gob/movil/info/About.java
+++ src/gob/movil/info/About.java
@@ -27,7 +27,7 @@ public class About extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
+		 setContentView(R.layout.about);
 	}
 
 	public void onOkClick(View button) {
