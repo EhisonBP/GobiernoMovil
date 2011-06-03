ccb4e7f71caef4cd60bfd8c206b4609664bf60c1
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-03 11:54:47 -0430

    Modificando los patrones de búsqueda de la aplicación para adaptarse al nuevo formato de la base de datos de la aplicación.

--- src/gob/movil/info/Search.java
+++ src/gob/movil/info/Search.java
@@ -16,209 +16,151 @@
  * along with this program. If not, see http://www.gnu.org/licenses
  */
 
-//DEPRECATED
+package gob.movil.info;
 
-//package gob.movil.info;
-//
-//import gob.movil.R;
-//
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.List;
-//
-//import android.app.Dialog;
-//import android.app.ListActivity;
-//import android.app.SearchManager;
-//import android.content.Intent;
-//import android.database.Cursor;
-//import android.database.sqlite.SQLiteDatabase;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.view.View.OnClickListener;
-//import android.widget.AdapterView;
-//import android.widget.AdapterView.OnItemClickListener;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.ListView;
-//import android.widget.TextView;
-//
-//public class Search extends ListActivity {
-//	String TABLE1 = "alcaldias";
-//	String TABLE2 = "tramites";
-//	String TABLE3 = "funcionarios";
-//	Show dialog = new Show();
-//
-//	@Override
-//	public void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.search);
-//
-//		DatabaseHelper dbhelp = new DatabaseHelper(this);
-//		final SQLiteDatabase db = dbhelp.getReadableDatabase();
-//
-//		Intent queryIntent = getIntent();
-//		String queryAction = queryIntent.getAction();
-//
-//		if (Intent.ACTION_SEARCH.equals(queryAction)) {
-//			String searchKeywords = queryIntent
-//					.getStringExtra(SearchManager.QUERY);
-//			Cursor a = db.rawQuery(
-//					"SELECT * FROM " + TABLE1 + " WHERE "
-//							+ replace("estado_alc", searchKeywords) + " OR "
-//							+ replace("nombre_alc", searchKeywords) + " OR "
-//							+ replace("cargo_alc", searchKeywords), null);
-//
-//			Cursor t = db.rawQuery(
-//					"SELECT * FROM " + TABLE2 + " WHERE "
-//							+ replace("nombre_tra", searchKeywords) + " OR "
-//							+ replace("organismo_tra", searchKeywords), null);
-//
-//			Cursor f = db.rawQuery(
-//					"SELECT * FROM " + TABLE3 + " WHERE ("
-//							+ replace("ente_fun", searchKeywords) + " OR "
-//							+ replace("nombre_fun", searchKeywords) + " OR "
-//							+ replace("cargo_fun", searchKeywords)
-//							+ ") AND poder_fun != 7", null);
-//
-//			if (!a.moveToFirst() && !t.moveToFirst() && !f.moveToFirst()) {
-//				TextView tv = (TextView) findViewById(R.id.search_result);
-//				tv.setText(getString(R.string.search_none, searchKeywords));
-//				tv.setVisibility(0);
-//			} else {
-//				String[] array1 = showResults(a, 1), array2 = showResults(t, 2), array3 = showResults(
-//						f, 3);
-//				if (a.moveToFirst()) {
-//					array1 = showResults(a, 1);
-//				}
-//				if (t.moveToFirst()) {
-//					array2 = showResults(t, 2);
-//				}
-//				if (f.moveToFirst()) {
-//					array3 = showResults(f, 3);
-//				}
-//				String[] list = mergeArrays(array1, array2, array3);
-//				onResultSelected(list);
-//			}
-//		}
-//	}
-//
-//	public String replace(String col, String key) {
-//		String replacement = "REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER("
-//				+ col
-//				+ "),'á','a'), 'é','e'),'í','i'),'ó','o'),'ú','u'),'ñ','n') LIKE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER('%"
-//				+ key
-//				+ "%'),'á','a'), 'é','e'),'í','i'),'ó','o'),'ú','u'),'ñ','n')";
-//		return replacement;
-//	}
-//
-//	public String[] mergeArrays(String[] first, String[] second, String[] third) {
-//		List<String> all = new ArrayList<String>(first.length + second.length
-//				+ third.length);
-//		Collections.addAll(all, first);
-//		Collections.addAll(all, second);
-//		Collections.addAll(all, third);
-//		return all.toArray(new String[] {});
-//	}
-//
-//	public String[] showResults(Cursor c, int n) {
-//		String[] items = new String[c.getCount()];
-//		int i = 0;
-//		if (c.moveToFirst()) {
-//
-//			do {
-//				switch (n) {
-//				case 1:
-//					items[i] = c.getString(3) + "\n" + c.getString(4);
-//					break;
-//				case 2:
-//					items[i] = c.getString(1) + "\n"
-//							+ getString(R.string.tramites);
-//					break;
-//				case 3:
-//					items[i] = c.getString(2) + "\n" + c.getString(3);
-//					break;
-//				}
-//				i++;
-//			} while (c.moveToNext());
-//		}
-//		return items;
-//	}
-//
-//	public void onResultSelected(String[] listResults) {
-//		DatabaseHelper dbhelp = new DatabaseHelper(this);
-//		final SQLiteDatabase db = dbhelp.getReadableDatabase();
-//
-//		final ListView lv = (ListView) findViewById(R.id.list_result);
-//		ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
-//				android.R.layout.simple_list_item_1, listResults);
-//		lv.setAdapter(listAdapter);
-//
-//		lv.setOnItemClickListener(new OnItemClickListener() {
-//			public void onItemClick(AdapterView<?> a, View v, int position,
-//					long id) {
-//				String querySelected = lv.getItemAtPosition(position)
-//						.toString();
-//
-//				String[] querySplitted = querySelected.split("\n");
-//
-//				Cursor alc = db.rawQuery("SELECT * FROM " + TABLE1 + " WHERE "
-//						+ replace("nombre_alc", querySplitted[0]) + " AND "
-//						+ replace("cargo_alc", querySplitted[1]), null);
-//				if (alc.moveToFirst()) {
-//					if (lv.getItemAtPosition(position).equals(
-//							alc.getString(3) + "\n" + alc.getString(4))) {
-//						dialog.setDialog(Search.this, alc);
-//					}
-//				}
-//
-//				Cursor tra = db.rawQuery("SELECT * FROM " + TABLE2 + " WHERE "
-//						+ replace("nombre_tra", querySplitted[0]), null);
-//				if (tra.moveToFirst()) {
-//					if (lv.getItemAtPosition(position).equals(
-//							tra.getString(1) + "\n"
-//									+ getString(R.string.tramites))) {
-//						final Dialog dialog = new Dialog(Search.this);
-//						dialog.setContentView(R.layout.show);
-//						dialog.setTitle(tra.getString(1));
-//
-//						dialog.setCancelable(true);
-//						dialog.setCanceledOnTouchOutside(true);
-//						TextView text = (TextView) dialog
-//								.findViewById(R.id.public_official);
-//						text.setText(getString(R.string.organism) + ": "
-//								+ tra.getString(2) + ".\n\n"
-//								+ getString(R.string.requirements) + ":\n"
-//								+ tra.getString(3) + ".\n\n"
-//								+ getString(R.string.hours) + ": "
-//								+ tra.getString(4) + ".\n\n"
-//								+ getString(R.string.address) + ": "
-//								+ tra.getString(5) + ".\n\n"
-//								+ getString(R.string.cost) + ": "
-//								+ tra.getString(6) + ".\n\n"
-//								+ getString(R.string.info) + ": "
-//								+ tra.getString(7) + ".\n");
-//
-//						Button button = (Button) dialog.findViewById(R.id.ok);
-//
-//						button.setOnClickListener(new OnClickListener() {
-//							public void onClick(View v) {
-//								dialog.dismiss();
-//							}
-//						});
-//
-//						dialog.show();
-//					}
-//				}
-//				Cursor fun = db.rawQuery("SELECT * FROM " + TABLE3 + " WHERE "
-//						+ replace("ente_fun", querySplitted[0]) + " AND "
-//						+ replace("nombre_fun", querySplitted[1]), null);
-//				if (fun.moveToFirst()) {
-//					if (lv.getItemAtPosition(position).equals(
-//							fun.getString(2) + "\n" + fun.getString(3))) {
-//						dialog.setDialog(Search.this, fun);
-//					}
-//				}
-//			}
-//		});
-//	}
-//}
+import gob.movil.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Search extends ListActivity {
+	private static final String WHERE = " WHERE ";
+	private static final String OR = " OR ";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+
+		DatabaseHelper dbHelper = new DatabaseHelper(this);
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+		Intent queryIntent = getIntent();
+		String queryAction = queryIntent.getAction();
+
+		if (Intent.ACTION_SEARCH.equalsIgnoreCase(queryAction)) {
+			String searchKeywords = queryIntent
+					.getStringExtra(SearchManager.QUERY);
+			Cursor searchAgencies = db.rawQuery(
+					dbHelper.SELECT + dbHelper.AGENCIES + WHERE
+							+ replace("name", searchKeywords) + OR
+							+ replace("officer", searchKeywords) + OR
+							+ replace("office", searchKeywords) + OR
+							+ replace("twitter", searchKeywords), null);
+
+			Cursor searchMayoralties = db.rawQuery(
+					dbHelper.SELECT + dbHelper.MAYORALTIES + WHERE
+							+ replace("name", searchKeywords) + OR
+							+ replace("mayor", searchKeywords) + OR
+							+ replace("office", searchKeywords) + OR
+							+ replace("twitter", searchKeywords), null);
+
+			Cursor searchProcedures = db.rawQuery(
+					dbHelper.SELECT + dbHelper.PROCEDURES + WHERE
+							+ replace("name", searchKeywords), null);
+
+			if (!searchAgencies.moveToFirst()
+					&& !searchMayoralties.moveToFirst()
+					&& !searchProcedures.moveToFirst()) {
+				Resources res = getResources();
+				String text = String.format(
+						res.getString(R.string.search_none), searchKeywords);
+				CharSequence styledText = Html.fromHtml(text);
+
+				TextView noFound = (TextView) findViewById(R.id.search_result);
+				noFound.setText(styledText);
+				noFound.setVisibility(0);
+			} else {
+				String[] resultsAgencies = listResults(searchAgencies);
+				String[] resultsMayoralties = listResults(searchMayoralties);
+				String[] resultsProcedures = listResults(searchProcedures);
+
+				String[] results = mergeArrays(resultsAgencies,
+						resultsMayoralties, resultsProcedures);
+
+				onResultClick(results);
+			}
+		}
+	}
+
+	public String replace(String column, String keyword) {
+		String replacement = "REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE("
+				+ column
+				+ ",'á','a'), 'é','e'),'í','i'),'ó','o'),'ú','u'),'ñ','n') LIKE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER('%"
+				+ keyword
+				+ "%'),'á','a'), 'é','e'),'í','i'),'ó','o'),'ú','u'),'ñ','n')";
+		return replacement;
+	}
+
+	public String[] listResults(Cursor list) {
+		int i = 0;
+		String[] items = new String[list.getCount()];
+		if (list.moveToFirst()) {
+			do {
+				items[i] = list.getString(1);
+				i++;
+			} while (list.moveToNext());
+		}
+		return items;
+	}
+
+	private String[] mergeArrays(String[] a, String[] b, String[] c) {
+		List<String> all = new ArrayList<String>(a.length + b.length + c.length);
+		Collections.addAll(all, a);
+		Collections.addAll(all, b);
+		Collections.addAll(all, c);
+		return all.toArray(new String[] {});
+	}
+
+	public void onResultClick(String[] results) {
+		final DatabaseHelper dbHelper = new DatabaseHelper(this);
+		final ListView resultsList = (ListView) findViewById(R.id.list_result);
+		ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, results);
+		resultsList.setAdapter(listAdapter);
+
+		resultsList.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> a, View v, int position,
+					long id) {
+				Show widgets = new Show();
+				String itemClicked = resultsList.getItemAtPosition(position)
+						.toString();
+				Cursor queryAgencies = queryItemClicked(dbHelper.AGENCIES,
+						itemClicked);
+				Cursor queryMayoralties = queryItemClicked(
+						dbHelper.MAYORALTIES, itemClicked);
+				Cursor queryProcedures = queryItemClicked(dbHelper.PROCEDURES,
+						itemClicked);
+				//Solve this problem.
+//				widgets.setDialog(Search.this, queryAgencies);
+//				widgets.setDialog(Search.this, queryMayoralties);
+				//This one is not working.
+//				widgets.setProceduresDialog(Search.this, queryProcedures);
+			}
+		});
+	}
+
+	private Cursor queryItemClicked(String table, String item) {
+		DatabaseHelper dbHelper = new DatabaseHelper(this);
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		Cursor query = db.rawQuery(dbHelper.SELECT + table + WHERE + "name = '"
+				+ item + "'", null);
+		return query;
+	}
+}
