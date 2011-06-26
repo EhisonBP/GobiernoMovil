46d8c0489dff058288c3fd41b5c5dd86c7804ec4
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-26 08:24:01 -0430

    Agregando nuevo archivo para el manejo de las constantes en la base de datos y minimizar la interacción con el archivo que contiene la lógica de la base de datos.

---
+++ src/gob/movil/info/Constants.java
@@ -0,0 +1,13 @@
+package gob.movil.info;
+
+public interface Constants {
+	public static final String POWERS = "powers";
+	public static final String STATES = "states";
+	public static final String MUNICIPALITIES = "municipalities";
+	public static final String AGENCIES = "agencies";
+	public static final String PROCEDURES = "procedures";
+	public static final String MAYORALTIES = "mayoralties";
+	
+	public final String INSERT = "INSERT INTO ";
+	public final String SELECT = "SELECT * FROM ";
+}
