813c1c6f21e59ce31c0871754cbb9e0c3b56f13b
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-27 10:00:20 -0430

    Agregando una interfaz para cubrir las llaves de la aplicación registrada en Twitter. Este archivo no debe pasar con las llaves reales a través del control de versiones. Pendiente.

---
+++ src/gob/movil/twitter/Keys.java
@@ -0,0 +1,6 @@
+package gob.movil.twitter;
+
+public interface Keys {
+	public static final String CONSUMER_KEY = "Your consumer key here";
+	public static final String CONSUMER_SECRET = "Your consumer secret here";
+}
