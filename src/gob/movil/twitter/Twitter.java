7fa7167e2c01f9c4665d59542cea6ee255fd0ceb
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-27 15:58:08 -0430

    Agregando el módulo principal para la comunicación con la interfaz gráfica del cliente Twitter en la aplicación. Tendrá la lógica para utilizar la red social y será la primera instancia en mostrarse si el usuario entra por primera vez a configurar su cuenta o enviar un Tweet.

---
+++ src/gob/movil/twitter/Twitter.java
@@ -0,0 +1,12 @@
+package gob.movil.twitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Twitter extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.twitter);
+	}
+}
