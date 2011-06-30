015a2b83c28c8d9d9486519c686c98b4500452ff
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-30 16:01:33 -0430

    Agregando comentarios al código fuente de Gobierno Móvil, la Activity principal.

--- src/gob/movil/app/GobiernoMovil.java
+++ src/gob/movil/app/GobiernoMovil.java
@@ -13,13 +13,13 @@ import android.view.MenuItem;
 import android.view.View;
 
 public class GobiernoMovil extends Activity {
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
 
+	/** Creamos el menú de opciones. */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
@@ -27,6 +27,7 @@ public class GobiernoMovil extends Activity {
 		return true;
 	}
 
+	/** Opción seleccionada. */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -44,6 +45,7 @@ public class GobiernoMovil extends Activity {
 		}
 	}
 
+	/** Vista seleccionada. */
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.main_dir:
@@ -58,6 +60,7 @@ public class GobiernoMovil extends Activity {
 		}
 	}
 
+	/** Mostramos la opción seleccionada. */
 	public void showActivity(Class<?> c) {
 		Intent intent = new Intent();
 		intent.setComponent(new ComponentName(this, c));
