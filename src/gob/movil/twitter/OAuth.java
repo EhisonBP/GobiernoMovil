803a46eda61d7c11d8b748af85a8c57b04359467
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-06-30 11:37:56 -0430

    Preparando las variables a utilizar para la autenticación.

--- src/gob/movil/twitter/OAuth.java
+++ src/gob/movil/twitter/OAuth.java
@@ -1,10 +1,14 @@
 package gob.movil.twitter;
 
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class OAuth extends Activity {
 	private String CALLBACK_URL = "callback://gm";
+	private OAuthProvider provider;
+	private CommonsHttpOAuthConsumer consumer;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
