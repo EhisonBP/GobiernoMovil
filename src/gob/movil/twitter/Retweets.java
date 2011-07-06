962c10e8d55b78956219c0306b5f4e9f9b0da2e6
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-07-06 17:17:21 -0430

    Agregando el código fuente para controlar las interfaces gráficas de las "menciones" y los "retweets".

---
+++ src/gob/movil/twitter/Retweets.java
@@ -0,0 +1,13 @@
+package gob.movil.twitter;
+
+import gob.movil.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Retweets extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.retweets);
+	}
+}
