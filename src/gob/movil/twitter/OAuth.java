6f696aa671627df49e04402ad7f8ff196e58c514
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-07-10 19:57:30 -0430

    Agregando nuevas constantes con las direcciones a las API de Twitter.

--- src/gob/movil/twitter/OAuth.java
+++ src/gob/movil/twitter/OAuth.java
@@ -25,12 +25,31 @@ import android.os.Bundle;
 
 public class OAuth extends Activity {
 	private String CALLBACK_URL = "callback://gm";
+	private String REQUEST_TOKEN_URL = "http://api.twitter.com/oauth/request_token";
+	private String ACCESS_TOKEN_URL = "http://api.twitter.com/oauth/access_token";
+	private String AUTHORIZE_URL = "http://api.twitter.com/oauth/authorize";
 	private OAuthProvider provider;
 	private CommonsHttpOAuthConsumer consumer;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.oauth);
 	}
+
+	// public void askOAuth() {
+	// try {
+	// consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY,
+	// CONSUMER_SECRET);
+	// provider = new DefaultOAuthProvider(REQUEST_TOKEN_URL,
+	// ACCESS_TOKEN_URL, AUTHORIZE_URL);
+	// String authURL = provider.retrieveRequestToken(consumer,
+	// CALLBACK_URL);
+	// Toast.makeText(this, "Please authorize this app!",
+	// Toast.LENGTH_LONG).show();
+	// // setConsumerProvider();
+	// startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authURL)));
+	// } catch (Exception e) {
+	// Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+	// }
+	// }
 }
