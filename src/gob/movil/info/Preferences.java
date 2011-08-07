9c94a0ab15b556d3f3c5ee56776447e933bf8a88
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-08-06 19:32:20 -0430

    Agregando nuevos métodos para el manejo de las preferencias de eliminar el historial de búsqueda y permitir usarlo. Incluye una alerta de diálogo.

--- src/gob/movil/info/Preferences.java
+++ src/gob/movil/info/Preferences.java
@@ -19,24 +19,80 @@
 package gob.movil.info;
 
 import gob.movil.R;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.provider.SearchRecentSuggestions;
+import android.widget.Toast;
 
 public class Preferences extends PreferenceActivity {
 	private static final String OPT_VIBRATION = "vibration";
 	private static final boolean OPT_VIBRATION_DEF = true;
+	private static final String OPT_SEARCH = "search";
+	private static final boolean OPT_SEARCH_DEF = false;
+	private static final String OPT_SUGGESTIONS = "suggestions";
+	private static final boolean OPT_SUGGESTIONS_DEF = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		getPreferenceManager().findPreference(OPT_SEARCH)
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						alertDialog();
+						return false;
+					}
+				});
 	}
 
-	/** Obtenemos el valor actual de la opción de vibración */
+	/** Obtenemos el valor actual de la opción de vibración. */
 	public static boolean getVibration(Context context) {
 		return PreferenceManager.getDefaultSharedPreferences(context)
 				.getBoolean(OPT_VIBRATION, OPT_VIBRATION_DEF);
+	}
+
+	/** Obtenemos el valor actual de la opción de sugerencias. */
+	public static boolean getSuggestions(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getBoolean(OPT_SUGGESTIONS, OPT_SUGGESTIONS_DEF);
+	}
+
+	private void alertDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(getString(R.string.clear_history_dialog))
+				.setCancelable(false)
+				.setPositiveButton(getString(R.string.yes),
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								clearSuggestions();
+								Toast.makeText(getApplicationContext(),
+										getString(R.string.cleared),
+										Toast.LENGTH_SHORT).show();
+								getPreferenceManager().findPreference(
+										OPT_SEARCH).setEnabled(OPT_SEARCH_DEF);
+							}
+						})
+				.setNegativeButton(getString(R.string.no),
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+	/** Borra el historial de búsquedas realizadas. */
+	public void clearSuggestions() {
+		SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+				SuggestionProvider.AUTHORITY, SuggestionProvider.MODE);
+		suggestions.clearHistory();
 	}
 }
