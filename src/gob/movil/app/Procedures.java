764ab95c990a67081fc25f0bfc28c4c82442b0d2
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-08-07 11:38:53 -0430

    Eliminando espacios, líneas en blanco, métodos y variables globales que ahora serán utilizados por herencia desde la súper clase Main.

--- src/gob/movil/app/Procedures.java
+++ src/gob/movil/app/Procedures.java
@@ -25,35 +25,24 @@ import gob.movil.R;
 import gob.movil.info.DatabaseHelper;
 import gob.movil.info.Preferences;
 import gob.movil.info.Show;
-import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Vibrator;
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Procedures extends Main {
-	private DatabaseHelper helper;
-	private SQLiteDatabase db;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.procedures);
-
 		Intent receive = getIntent();
 		final int procedure = receive.getIntExtra("item", 0);
-
 		helper = new DatabaseHelper(this);
 		db = helper.getReadableDatabase();
-
 		Cursor procedures = db.rawQuery(SELECT + PROCEDURES, null);
-
 		String[] items = new String[procedures.getCount()];
 		int i = 0;
 		if (procedures.moveToFirst()) {
@@ -62,15 +51,12 @@ public class Procedures extends Main {
 				i++;
 			} while (procedures.moveToNext());
 		}
-
 		final Spinner spinner = (Spinner) findViewById(R.id.spinner_procedures);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_spinner_item, items);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
-
 		spinner.setSelection(procedure - 1);
-
 		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			public void onItemSelected(AdapterView<?> parent,
 					android.view.View v, int position, long id) {
@@ -91,11 +77,10 @@ public class Procedures extends Main {
 				}
 			}
 
-			public void onNothingSelected(AdapterView<?> arg0) {
+			public void onNothingSelected(AdapterView<?> v) {
 				// TODO Auto-generated method stub
 			}
 		});
-
 	}
 
 	public void setTextProcedure(Cursor query) {
@@ -108,15 +93,5 @@ public class Procedures extends Main {
 				+ getString(R.string.info) + ": " + query.getString(5)
 				+ ".\n\n" + getString(R.string.organism) + ": "
 				+ query.getString(6) + ".");
-	}
-
-	public void onMainClick(View button) {
-		finish();
-	}
-
-	/** Alerta vibrante. */
-	public void setVibration(int miliSeconds) {
-		Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-		v.vibrate(miliSeconds);
 	}
 }
