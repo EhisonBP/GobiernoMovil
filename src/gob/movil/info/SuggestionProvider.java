ca77d018bd10fff7973529f0dcf382079b19964b
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-08-06 19:33:36 -0430

    Agregando un archivo que servirá para manejar las sugerencias de búsqueda.

---
+++ src/gob/movil/info/SuggestionProvider.java
@@ -0,0 +1,30 @@
+/*
+ * Licencia GPL v3
+ * "Gobierno Móvil" es un producto de "Gobierno en Línea".
+ * Copyright (C) 2011 Richard Ricciardelli. Centro Nacional de Tecnologías de Información. All Rights Reserved.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses
+ */
+
+package gob.movil.info;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+public class SuggestionProvider extends SearchRecentSuggestionsProvider {
+	public final static String AUTHORITY = "gob.movil.info.SuggestionProvider";
+	public final static int MODE = DATABASE_MODE_QUERIES;
+
+	public SuggestionProvider() {
+		setupSuggestions(AUTHORITY, MODE);
+	}
+}
