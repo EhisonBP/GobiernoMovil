5a3b1ae261a2c57ab94b7f34654020c180e85b39
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-08-24 21:40:48 -0430

    Cambiando la versión de la base de datos y removiendo espacios en blanco en el método para actualizar las tablas de la base de datos.

--- src/gob/movil/info/DatabaseHelper.java
+++ src/gob/movil/info/DatabaseHelper.java
@@ -32,7 +32,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 	private static final String DB_NAME = "gm.db";
-	private static final int DB_VERSION = 2;
+	private static final int DB_VERSION = 5;
 	
 	public DatabaseHelper(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
@@ -521,25 +521,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		
 		android.util.Log.v(POWERS, "Upgrading database, which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + POWERS);
-
 		android.util.Log.v(STATES, "Upgrading database, which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + STATES);
-
 		android.util.Log.v(MUNICIPALITIES, "Upgrading database, which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + MUNICIPALITIES);
-		
 		android.util.Log.v(AGENCIES, "Upgrading database, which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + AGENCIES);
-		
 		android.util.Log.v(PROCEDURES, "Upgrading database, which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + PROCEDURES);
-		
 		android.util.Log.v(MAYORALTIES, "Upgrading database, which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + MAYORALTIES);
-
 		onCreate(db);
 	}
 }
