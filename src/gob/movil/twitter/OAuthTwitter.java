5c610d1bc8a65bd7eb8413aeb66540d1b6ff4937
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-09-01 16:05:26 -0430

    Agregando la lógica para la autenticación con Twitter.

--- src/gob/movil/twitter/OAuthTwitter.java
+++ src/gob/movil/twitter/OAuthTwitter.java
@@ -20,13 +20,17 @@ package gob.movil.twitter;
 
 import static gob.movil.info.Constants.CONSUMER_KEY;
 import static gob.movil.info.Constants.CONSUMER_SECRET;
+import static gob.movil.info.Constants.TWITTER_PREFERENCES;
 import gob.movil.R;
 import oauth.signpost.OAuthProvider;
 import oauth.signpost.basic.DefaultOAuthProvider;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.http.AccessToken;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -48,6 +52,8 @@ public class OAuthTwitter extends Activity {
 		System.setProperty("http.keepAlive", "false");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.oauth);
+		checkCredentials();
+		getConsumerProvider();
 		Button authentication = (Button) findViewById(R.id.authenticate);
 		authentication.setOnClickListener(new OnClickListener() {
 			@Override
@@ -55,6 +61,42 @@ public class OAuthTwitter extends Activity {
 				getOAuth();
 			}
 		});
+	}
+
+	private void checkCredentials() {
+		// TODO @method
+		AccessToken access = getAccessToken();
+		if (access == null)
+			return;
+		twitter = new TwitterFactory().getInstance();
+		twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+		twitter.setOAuthAccessToken(access);
+		((TwitterApplication) getApplication()).setTwitter(twitter);
+		Intent i = new Intent(this, TwitterActivity.class);
+		startActivity(i);
+		finish();
+	}
+
+	private AccessToken getAccessToken() {
+		SharedPreferences preferences = getSharedPreferences(
+				TWITTER_PREFERENCES, MODE_PRIVATE);
+		String token = preferences.getString("token", "");
+		String secret = preferences.getString("secret", "");
+		if (token != null && secret != null && !"".equals(secret)
+				&& !"".equals(token)) {
+			return new AccessToken(token, secret);
+		}
+		return null;
+	}
+
+	private void getConsumerProvider() {
+		OAuthProvider p = ((TwitterApplication) getApplication()).getProvider();
+		CommonsHttpOAuthConsumer c = ((TwitterApplication) getApplication())
+				.getConsumer();
+		if (p != null)
+			provider = p;
+		if (c != null)
+			consumer = c;
 	}
 
 	private void getOAuth() {
@@ -66,11 +108,59 @@ public class OAuthTwitter extends Activity {
 			String url = provider.retrieveRequestToken(consumer, CALLBACK_URL);
 			Toast.makeText(getApplicationContext(),
 					getString(R.string.authorize), Toast.LENGTH_SHORT);
+			setConsumerProvider();
 			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
 		} catch (Exception e) {
 			Toast.makeText(getApplicationContext(),
 					getString(R.string.authentication_error),
 					Toast.LENGTH_SHORT);
 		}
+	}
+
+	private void setConsumerProvider() {
+		if (provider != null)
+			((TwitterApplication) getApplication()).setProvider(provider);
+		if (consumer != null)
+			((TwitterApplication) getApplication()).setConsumer(consumer);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (this.getIntent() != null && this.getIntent().getData() != null) {
+			Uri uri = this.getIntent().getData();
+			if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {
+				String verifier = uri
+						.getQueryParameter(oauth.signpost.OAuth.OAUTH_VERIFIER);
+				try {
+					provider.retrieveAccessToken(consumer, verifier);
+					AccessToken a = new AccessToken(consumer.getToken(),
+							consumer.getTokenSecret());
+					storeAccessToken(a);
+					//
+					twitter = new TwitterFactory().getInstance();
+					twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+					twitter.setOAuthAccessToken(a);
+					((TwitterApplication) getApplication()).setTwitter(twitter);
+					Intent i = new Intent(this, TwitterActivity.class);
+					startActivity(i);
+					finish();
+				} catch (Exception e) {
+					e.printStackTrace();
+					Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG)
+							.show();
+				}
+			}
+
+		}
+	}
+
+	private void storeAccessToken(AccessToken access) {
+		SharedPreferences preferences = getSharedPreferences(
+				TWITTER_PREFERENCES, MODE_PRIVATE);
+		SharedPreferences.Editor editor = preferences.edit();
+		editor.putString("access", access.getToken());
+		editor.putString("secret", access.getTokenSecret());
+		editor.commit();
 	}
 }
