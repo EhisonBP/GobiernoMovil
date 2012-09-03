26ab8109974435a0d7f37b972875e223179d558a
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-09-03 15:36:58 -0430

    Agregando el archivo principal modificado.

--- src/gob/movil/app/Main.java
+++ src/gob/movil/app/Main.java
@@ -1,38 +1,18 @@
-/*
- * Licencia GPL v3
- * "Gobierno Móvil" es un producto de "Gobierno en Línea".
- * Copyright (C) 2011 Richard Ricciardelli. Centro Nacional de Tecnologías de Información. All Rights Reserved.
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any
- * later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see http://www.gnu.org/licenses
- */
-
 package gob.movil.app;
 
-import static android.provider.BaseColumns._ID;
-import static gob.movil.info.Constants.INSERT;
-import static gob.movil.info.Constants.TWITTER_PREFERENCES;
-import static gob.movil.info.Constants.VIBRATION_INTENT;
 import gob.movil.R;
 import gob.movil.info.About;
+import gob.movil.info.Constants;
 import gob.movil.info.DatabaseHelper;
 import gob.movil.info.Help;
 import gob.movil.info.Preferences;
-import gob.movil.twitter.OAuthTwitter;
-import gob.movil.twitter.TwitterActivity;
+import gob.movil.info.Update;
+
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -42,10 +22,58 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class Main extends Activity {
-	private static DatabaseHelper helper;
+public class Main extends Activity implements Constants {
+
+	public static DatabaseHelper helper;
 	private static SQLiteDatabase db;
 
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		crearBBDD();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	/** creando el menu. */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	/** Seleccionar opcion. */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.about:
+			Intent about = new Intent(this, About.class);
+			startActivity(about);
+			break;
+		case R.id.help:
+			Intent help = new Intent(this, Help.class);
+			startActivity(help);
+			break;
+		case R.id.update:
+			Intent update = new Intent(this, Update.class);
+			startActivity(update);
+			break;
+		case R.id.preferences:
+			Intent preferences = new Intent(this, Preferences.class);
+			startActivity(preferences);
+			break;
+		case R.id.search:
+			onSearchRequested();
+			break;
+		}
+		return false;
+	}
+
+	/**
+	 * metodo para abrir la base de datos
+	 * 
+	 */
 	public static void openDatabase(Context context) {
 		helper = new DatabaseHelper(context);
 		db = helper.getWritableDatabase();
@@ -58,7 +86,7 @@ public class Main extends Activity {
 
 	public String[] getListItems(Context context, String query) {
 		openDatabase(context);
-		Cursor cursor = db.rawQuery("SELECT name FROM " + query, null);
+		Cursor cursor = db.rawQuery("SELECT nombre FROM " + query, null);
 		String[] items = new String[cursor.getCount()];
 		short i = 0;
 		if (cursor.moveToFirst())
@@ -69,16 +97,6 @@ public class Main extends Activity {
 		cursor.close();
 		closeDatabase();
 		return items;
-	}
-
-	public void insertValues(Context context, String table, String[] array) {
-		openDatabase(context);
-		db.execSQL("DROP TABLE IF EXISTS " + table);
-		db.execSQL("CREATE TABLE " + table + "(" + _ID
-				+ " INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)");
-		for (String element : array)
-			db.execSQL(INSERT + table + " (name) VALUES (\"" + element + "\")");
-		closeDatabase();
 	}
 
 	public String[] getArrayFromCursor(Context context, String query) {
@@ -95,53 +113,6 @@ public class Main extends Activity {
 		return data;
 	}
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
-
-	/** Creating the menu. */
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu, menu);
-		return true;
-	}
-
-	/** Option selected. */
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.about:
-			showActivity(About.class, 0);
-			return true;
-		case R.id.help:
-			showActivity(Help.class, 0);
-			return true;
-		case R.id.search:
-			onSearchRequested();
-			return true;
-		case R.id.preferences:
-			showActivity(Preferences.class, 0);
-			return true;
-		case R.id.twitter:
-			SharedPreferences preferences = getSharedPreferences(
-					TWITTER_PREFERENCES, MODE_PRIVATE);
-			if (!preferences.getString("access", "").equalsIgnoreCase("")
-					|| !preferences.getString("secret", "")
-							.equalsIgnoreCase("")) {
-				showActivity(TwitterActivity.class, 0);
-			} else {
-				showActivity(OAuthTwitter.class, 0);
-			}
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
-	/** Selected view. */
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.directory_main:
@@ -156,7 +127,6 @@ public class Main extends Activity {
 		}
 	}
 
-	/** Show the activity. */
 	public void showActivity(Class<?> c, int item) {
 		if (Preferences.getVibration(getApplicationContext())) {
 			setVibration(VIBRATION_INTENT);
@@ -166,10 +136,6 @@ public class Main extends Activity {
 		startActivity(i);
 	}
 
-	/**
-	 * @param milliSeconds
-	 *            Set vibration in milliseconds.
-	 * */
 	public void setVibration(int milliSeconds) {
 		Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 		v.vibrate(milliSeconds);
@@ -177,5 +143,18 @@ public class Main extends Activity {
 
 	public void onMainClick(View button) {
 		finish();
+	}
+
+	/**
+	 * metodo para crear la base de datos dentro de la actividad princiapl
+	 * 
+	 */
+	public void crearBBDD() {
+		helper = new DatabaseHelper(this);
+		try {
+			helper.crearDataBase();
+		} catch (IOException ioe) {
+			throw new Error("Unable to create database");
+		}
 	}
 }
